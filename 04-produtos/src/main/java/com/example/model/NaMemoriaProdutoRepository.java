45ec317008e7dc8a20da4a75799bfc55271427a7
jeferson.zavaski <jeferson.zavaski@LCET1116.cwbdis.pucpr.br>
2016-04-06 19:41:19 -0300

    falta acabar o 4

--- 04-produtos/src/main/java/com/example/model/NaMemoriaProdutoRepository.java
+++ 04-produtos/src/main/java/com/example/model/NaMemoriaProdutoRepository.java
@@ -13,17 +13,24 @@ public class NaMemoriaProdutoRepository implements ProdutoRepository {
 		produtos.get(0).setId(1L);
 		produtos.get(0).setNome("Table Samsung");
 		produtos.get(0).setPreco(899.00);
+		produtos.get(0).setCategoria(new Categoria());
+		produtos.get(0).getCategoria().setId(1);
+		produtos.get(0).getCategoria().setNome("Eletrônica");
 		
 		produtos.add(new Produto());
 		produtos.get(1).setId(2L);
 		produtos.get(1).setNome("Celular Iphone 5s");
 		produtos.get(1).setPreco(4_500.00);
+		produtos.get(1).setCategoria(produtos.get(0).getCategoria());;
+
 		
 		produtos.add(new Produto());
 		produtos.get(2).setId(3L);
 		produtos.get(2).setNome("Livro Java Como Programar");
 		produtos.get(2).setPreco(400.00);
-		
+		produtos.get(2).setCategoria(new Categoria());
+		produtos.get(2).getCategoria().setId(2);
+		produtos.get(2).getCategoria().setNome("Livros");
 		
 		
 	}
